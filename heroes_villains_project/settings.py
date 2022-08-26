4bd99eff61c819d5ec7be1099f0a872f1eaa00cb
trevorwavelet@gmail.com <trevorwavelet@gmail.com>
2022-08-26 14:31:53 -0400

    Created the Supers and Super_Type apps for this project

--- heroes_villains_project/settings.py
+++ heroes_villains_project/settings.py
@@ -35,7 +35,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -111,3 +110,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+try:
+    from heroes_villains_project.local_settings import *
+except ImportError:
+    pass
