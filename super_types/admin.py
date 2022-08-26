821651d93704f2cc5548692ee998326e390e6087
trevorwavelet@gmail.com <trevorwavelet@gmail.com>
2022-08-26 17:36:53 -0400

    Created the admin site and superusers for both models + Wrote the logic for the views.py file for the Supers model -- GET, POST, PUT, DELETE + Updated the urls

--- super_types/admin.py
+++ super_types/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
-# Register your models here.
+from.models import SuperType
+# Register your models here
+admin.site.register(SuperType)
--- supers/views.py
+++ supers/views.py
@@ -4,3 +4,38 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import SupersSerializer
 from .models import Supers
+
+
+@api_view(['GET', 'POST'])
+def supers_list(request):
+    
+    if request.method == 'GET':
+        supers = Supers.objects.all()
+        serializer = SupersSerializer(supers, many = True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    elif request.method == 'POST':
+        serializer = SupersSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status =status.HTTP_201_CREATED)
+        
+
+@api_view(['GET', 'POST', 'DELETE'])
+def supers_detail(request, pk):
+
+    super = get_object_or_404(Supers, pk=pk)
+    if request.method == 'GET':
+        serializer = SupersSerializer(super)
+        return Response(serializer.data)
+
+    elif request.data == 'PUT':
+        serializer = SupersSerializer(super, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    elif request.data == 'DELETE':
+        super.delete()
+        return Response(status.HTTP_204_NO_CONTENT)
+
